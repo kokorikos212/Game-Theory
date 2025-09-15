8e0488677e8f26ad3d4333ab9d7657a2495dcf28
kokorikos212 <nospaspace@gmail.com>
2025-09-15 15:23:05 +0300

    Remove env file

--- genv/lib/python3.10/site-packages/nashpy/__init__.py
+++
@@ -1,5 +0,0 @@
-"""A library with algorithms on 2 player games."""
-
-from .game import Game
-
-__version__ = "0.0.41"
--- genv/lib/python3.10/site-packages/nashpy/polytope/__init__.py
+++
@@ -1,6 +0,0 @@
-from .polytope import (
-    build_halfspaces,
-    find_feasible_point,
-    labels,
-    non_trivial_vertices,
-)
